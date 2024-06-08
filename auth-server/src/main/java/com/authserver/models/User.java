0cd2c68752b6e74590cc712661d16a49d4d80342
JuanC <abjuancarlos.12@gmail.com>
2024-06-07 22:04:34 -0500

    Fix: update User model because it does not have an entity annotation (@Entity).

--- auth-server/src/main/java/com/authserver/models/User.java
+++ auth-server/src/main/java/com/authserver/models/User.java
@@ -1,12 +1,10 @@
 package com.authserver.models;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
@@ -15,6 +13,8 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @NoArgsConstructor
+@Entity
+@Data
 public class User {
 
     @Id
