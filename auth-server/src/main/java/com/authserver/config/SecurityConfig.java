117f11f594c8da5c50f2b338a9627743b1de25be
JuanC <abjuancarlos.12@gmail.com>
2024-06-08 23:33:09 -0500

    feat: create been for encrypt the password and a little refactoring in securityFilterChain

--- auth-server/src/main/java/com/authserver/config/SecurityConfig.java
+++ auth-server/src/main/java/com/authserver/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,13 +15,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()  // Deshabilitar CSRF para APIs REST
+                .csrf(AbstractHttpConfigurer::disable) // Deshabilitar CSRF para APIs REST
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/user/v1/user/").permitAll()  // Permitir acceso a la ruta de registro
+                        .requestMatchers("/user/**").permitAll()  // Permitir acceso a la ruta de registro
                         .anyRequest().authenticated()  // Requiere autenticación para cualquier otra solicitud
-                )
-                .httpBasic();
+                );
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder  passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
